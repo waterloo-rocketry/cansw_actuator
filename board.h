ad96c1580366ca839132ef9b857bf3b9cea1c197
Zachary Teper <zachary.teper@gmail.com>
2022-06-06 21:17:28 -0400

    Distinguish between vent and injector board safe states

--- board.h
+++ board.h
@@ -1,11 +1,26 @@
 #ifndef BOARD_H
 #define BOARD_H
 
-// Contains miscellaneous vent board-specific code
+// Contains miscellaneous board-specific code
 
+#define VENT_BOARD     (BOARD_UNIQUE_ID & 0x8)
+
+// If I am not a VENT, then I am an INJECTOR
+#if VENT_BOARD
+#define INJECTOR_BOARD 0
+#else
+#define INJECTOR_BOARD 1
+#endif
+
+#if INJECTOR_BOARD
 #define SAFE_STATE ACTUATOR_OPEN
+#define ACTUATOR_ID ACTUATOR_INJECTOR_VALVE
+#define BATTERY_SENSOR_ID SENSOR_INJ_BATT
+#elif VENT_BOARD
+#define SAFE_STATE ACTUATOR_CLOSE
 #define ACTUATOR_ID ACTUATOR_VENT_VALVE
 #define BATTERY_SENSOR_ID SENSOR_VENT_BATT
+#endif
 
 #define MAX_LOOP_TIME_DIFF_ms 250
 
